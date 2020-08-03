64358dc2e825495283b18621a8adc7f884e4bf93
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2020-08-03 10:12:37 +0530

    Create .git stubs - stage 1

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
-#[allow(unused_imports)]
 use std::env;
-#[allow(unused_imports)]
 use std::fs;
 
 fn main() {
     println!("Your code goes here!");
 
-    // Uncomment this block to pass the first stage
-    // let args: Vec<String> = env::args().collect();
-    // if args[1] == "init" {
-    //     fs::create_dir(".git").unwrap();
-    //     fs::create_dir(".git/objects").unwrap();
-    //     fs::create_dir(".git/refs").unwrap();
-    //     fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
-    //     println!("Initialized git directory")
-    // } else {
-    //     println!("unknown command: {}", args[1])
-    // }
+    let args: Vec<String> = env::args().collect();
+    if args[1] == "init" {
+        fs::create_dir(".git").unwrap();
+        fs::create_dir(".git/objects").unwrap();
+        fs::create_dir(".git/refs").unwrap();
+        fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
+        println!("Initialized git directory")
+    } else {
+        println!("unknown command: {}", args[1])
+    }
 }
