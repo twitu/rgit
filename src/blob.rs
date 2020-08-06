49945f0884735e7381a3850cf8c90724164f2f72
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2020-08-06 05:47:15 +0530

    Hash file content + object header

--- src/blob.rs
+++ src/blob.rs
@@ -2,14 +2,14 @@ use flate2::read::ZlibDecoder;
 use flate2::write::ZlibEncoder;
 use flate2::Compression;
 use sha1::{Digest, Sha1};
-use std::fs::{read, File, create_dir_all};
+use std::fs::{create_dir_all, read, File};
 use std::io::Read;
 use std::path::PathBuf;
 
 // hack to import both Writes
 // https://stackoverflow.com/questions/59187608/can-i-use-write-and-file-write-all-in-same-fn
-use std::io::Write as _;
 use std::fmt::Write as _;
+use std::io::Write as _;
 
 pub fn read_blob(blob_sha: &String) -> () {
     let dir = &blob_sha[0..2];
@@ -40,27 +40,30 @@ pub fn hash_file(file_path: &String) -> () {
     }
 
     if let Ok(bytes) = read(path) {
+        // add meta data to file content
+        let meta_data = format!("blob {}", bytes.len().to_string());
+        let mut data = meta_data.into_bytes();
+        data.push(0); // append end string character
+        data.extend(bytes);
+
+        // hash data with sha1
         let mut hasher = Sha1::new();
-        hasher.update(&bytes);
+        hasher.update(&data);
         let result: [u8; 20] = hasher.finalize().into();
         let mut sha_val = String::with_capacity(result.len() * 2);
         for byte in &result {
             write!(sha_val, "{:02x}", byte);
         }
 
-        let meta_data = format!("blob {}", bytes.len().to_string());
-        let mut data = meta_data.into_bytes();
-        data.push(0); // append end string character
-        data.extend(bytes);
-
-        let blob_dir = &sha_val[0..2];
-        let blob_file = &sha_val[2..];
-        let blob_path: PathBuf = [".git", "objects", blob_dir, blob_file].iter().collect();
-
+        // compress data with zlib encoding
         let mut z = ZlibEncoder::new(Vec::new(), Compression::default());
         z.write_all(&data);
         let compressed = z.finish().unwrap();
 
+        // create path for storing blob
+        let blob_dir = &sha_val[0..2];
+        let blob_file = &sha_val[2..];
+        let blob_path: PathBuf = [".git", "objects", blob_dir, blob_file].iter().collect();
         create_dir_all(blob_path.parent().unwrap());
         let mut file = File::create(blob_path).unwrap();
         file.write_all(&compressed);
