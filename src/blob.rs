b3a1cc0c229e883d2775fdee827557474a049f25
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2020-08-10 21:00:56 +0530

    Recursively write tree object
    move object writing logic to separate function

--- src/blob.rs
+++ src/blob.rs
@@ -18,7 +18,39 @@ fn create_path_from_sha(sha: &String) -> PathBuf {
     path
 }
 
-pub fn read_blob(blob_sha: &String) {
+pub fn create_sha_string(sha: &[u8]) -> String {
+    let mut sha_str = String::with_capacity(sha.len() * 2);
+    for byte in sha {
+        write!(sha_str, "{:02x}", byte).unwrap();
+    }
+
+    sha_str
+}
+
+/// compress , hash and write blob to file
+/// return sha1 hash of blob
+fn write_blob(content: Vec<u8>) -> [u8; 20] {
+    // hash data with sha1
+    let mut hasher = Sha1::new();
+    hasher.update(&content);
+    let result: [u8; 20] = hasher.finalize().into();
+    let sha_val = create_sha_string(&result);
+
+    // compress data with zlib encoding
+    let mut z = ZlibEncoder::new(Vec::new(), Compression::default());
+    z.write_all(&content).unwrap();
+    let compressed = z.finish().unwrap();
+
+    // create path for storing blob
+    let blob_path = create_path_from_sha(&sha_val);
+    create_dir_all(blob_path.parent().unwrap()).unwrap();
+    let mut file = File::create(blob_path).unwrap();
+    file.write_all(&compressed).unwrap();
+
+    result
+}
+
+pub fn read_blob(blob_sha: &String) -> Option<String> {
     let path = create_path_from_sha(blob_sha);
 
     if let Ok(bytes) = read(path) {
@@ -29,16 +61,16 @@ pub fn read_blob(blob_sha: &String) {
         // strip blob meta data about size
         // http://shafiul.github.io/gitbook/1_the_git_object_model.html
         if let Some(i) = s.find('\x00') {
-            print!("{}", &s[i + 1..]);
+            Some(s[i + 1..].to_owned())
         } else {
-            print!("{}", s);
+            None
         }
     } else {
-        println!("blob {} does not exist.", blob_sha);
+        None
     }
 }
 
-pub fn hash_object(file_path: &String) -> () {
+pub fn hash_object(file_path: &str) -> Option<[u8; 20]> {
     let path = PathBuf::from(file_path);
     if !path.is_file() {
         println!("file {} does not exist", file_path);
@@ -46,33 +78,14 @@ pub fn hash_object(file_path: &String) -> () {
 
     if let Ok(bytes) = read(path) {
         // add meta data to file content
-        let meta_data = format!("blob {}", bytes.len().to_string());
-        let mut data = meta_data.into_bytes();
-        data.push(0); // append end string character
+        let mut data = format!("blob {}\x00", bytes.len().to_string()).into_bytes();
         data.extend(bytes);
 
         // hash data with sha1
-        let mut hasher = Sha1::new();
-        hasher.update(&data);
-        let result: [u8; 20] = hasher.finalize().into();
-        let mut sha_val = String::with_capacity(result.len() * 2);
-        for byte in &result {
-            write!(sha_val, "{:02x}", byte).unwrap();
-        }
-
-        // compress data with zlib encoding
-        let mut z = ZlibEncoder::new(Vec::new(), Compression::default());
-        z.write_all(&data).unwrap();
-        let compressed = z.finish().unwrap();
-
-        // create path for storing blob
-        let blob_path = create_path_from_sha(&sha_val);
-        create_dir_all(blob_path.parent().unwrap()).unwrap();
-        let mut file = File::create(blob_path).unwrap();
-        file.write_all(&compressed).unwrap();
-
-        // print sha to std out
-        print!("{}", sha_val);
+        let sha_val = write_blob(data);
+        Some(sha_val)
+    } else {
+        None
     }
 }
 
@@ -101,5 +114,71 @@ pub fn read_tree_object(tree_sha: &String) -> () {
         }
     } else {
         println!("Could not find object for sha {}", tree_sha);
+    }
+}
+
+struct TreeObject {
+    is_file: bool,
+    name: String,
+    sha_val: [u8; 20],
+}
+
+pub fn write_tree_object(dir_path: &str) -> Option<[u8; 20]> {
+    let path = PathBuf::from(dir_path);
+    let mut contents: Vec<TreeObject> = Vec::new();
+
+    if path.is_dir() {
+        for entry in path.read_dir().unwrap() {
+            if let Ok(dir_entry) = entry {
+                if let Ok(value) = dir_entry.file_type() {
+                    let name = dir_entry.file_name().to_str().unwrap().to_string();
+                    let path = dir_entry.path().to_str().unwrap().to_string();
+                    if !name.starts_with(".") {
+                        if value.is_file() {
+                            if let Some(sha_val) = hash_object(&path) {
+                                contents.push(TreeObject {
+                                    is_file: true,
+                                    name,
+                                    sha_val
+                                })
+                            }
+                        } else if value.is_dir() {
+                            if let Some(sha_val) = write_tree_object(&path) {
+                                contents.push(TreeObject {
+                                    is_file: false,
+                                    name,
+                                    sha_val
+                                })
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    if contents.is_empty() {
+        None
+    } else {
+        contents.sort_by(|o1, o2| o1.name.cmp(&o2.name));
+
+        // TODO: sha is stored in hex representation
+        let mut blob_content: Vec<u8> = Vec::new();
+        for content in contents {
+            let line = if content.is_file {
+                format!("100644 {}\x00", content.name)
+            } else {
+                format!("40000 {}\x00", content.name)
+            };
+            let mut line = line.into_bytes();
+            line.extend(content.sha_val.iter());
+            blob_content.extend(line);
+        }
+        let meta_data = format!("tree {}\x00", blob_content.len().to_string());
+        let mut blob_data = meta_data.into_bytes();
+        blob_data.extend(blob_content);
+
+        let sha_val = write_blob(blob_data);
+        Some(sha_val)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::fs;
 
-use git_starter_rust::blob::{hash_object, read_blob, read_tree_object};
+use git_starter_rust::blob::{hash_object, read_blob, read_tree_object, write_tree_object, create_sha_string};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -15,19 +15,28 @@ fn main() {
         if args.len() < 3 {
             println!("command usage: cat-file -p <blob-sha>")
         } else {
-            read_blob(&args[3]);
+            if let Some(content) = read_blob(&args[3]) {
+                print!("{}", content);
+            }
         }
     } else if args[1] == "hash-object" && args[2] == "-w" {
         if args.len() < 3 {
             println!("command usage: hash-object -w <file-name>")
         } else {
-            hash_object(&args[3]);
+            if let Some(sha_val) = hash_object(&args[3]) {
+                println!("{}", create_sha_string(&sha_val));
+            }
         }
     } else if args[1] == "ls-tree" && args[2] == "--name-only" {
         if args.len() < 3 {
             println!("command usage: ls-tree --name-only <tree-sha>")
         } else {
             read_tree_object(&args[3]);
+        }
+    } else if args[1] == "write-tree" {
+        let dir_path = env::current_dir().unwrap();
+        if let Some(sha_val) = write_tree_object(dir_path.to_str().unwrap()) {
+            println!("{}", create_sha_string(&sha_val));
         }
     }
 }
